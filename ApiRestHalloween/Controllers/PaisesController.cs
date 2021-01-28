5f4b1956e66bf9eaf6cf768d888f29c468d0b461
David Galarza <vatayo14@gmail.com>
2021-01-27 22:49:32 -0500

    Metodo ObtenerPaises

---
+++ ApiRestHalloween/Controllers/PaisesController.cs
@@ -0,0 +1,28 @@
+﻿using ApiRestHalloween.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiRestHalloween.Controllers
+{
+    public class PaisesController : ApiController
+    {
+
+        // Clase de acceso a datos
+        private PaisesDAL data;
+        public PaisesController()
+        {
+            // Instanciar el objeto.
+            this.data = new PaisesDAL();
+        }
+
+        public IEnumerable<Pais> GetPaises()
+        {
+            return data.GetPaises();
+        }
+
+    }
+}
---
+++ ApiRestHalloween/Models/PaisesDAL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ApiRestHalloween.Models
+{
+    public class PaisesDAL
+    {
+
+        public PaisesDAL() { }
+        private string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        }
+
+        public List<Pais> GetPaises()
+        {
+            List<Pais> lista = new List<Pais>();
+            string sql = "SELECT * FROM PAIS";
+
+
+            using (SqlConnection connection = new
+            SqlConnection(GetConnectionString()))
+            {
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader =
+
+                    command.ExecuteReader(CommandBehavior.CloseConnection);
+                    while (reader.Read())
+
+                    {
+
+                        Pais category = new Pais();
+                        category.IdPais = reader["IdPais"].ToString();
+                        category.Nombre = reader["Nombre"].ToString();
+
+
+                        lista.Add(category);
+                    }
+
+                    reader.Close();
+
+                }
+            }
+            return lista;
+        }
+    }
+}
